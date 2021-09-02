69e1a04f5372df3486662a0d7357e4654fdf7341
Muhammadaziz <muhammadazizravshanbekov107@gmail.com>
2021-09-02 11:14:16 +0500

    Remove other files

--- src/Components/Main/Intro/Intro.jsx
+++ src/Components/Main/Intro/Intro.jsx
@@ -1,11 +1,19 @@
 import ThemeIcon from "../../Lib/ThemeIcon";
 import "./Intro.css";
+import Uzicon from "../../Lib/Uz";
+import Usaicon from "../../Lib/Usa";
 function Intro() {
   return (
     <section className="intro">
       <div className="intro__wrapper">
         <h2 className="intro__wrapper__heading">Home</h2>
 
+        <button type="button" className="uz">
+          <Uzicon />
+        </button>
+        <button type="button" className="en">
+          <Usaicon />
+        </button>
         <button className="theme-btn" type="button">
           <ThemeIcon />
         </button>
