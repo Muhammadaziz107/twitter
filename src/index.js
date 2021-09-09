af2a3dfad67ca4e363bbeef27c0dd29926c7e52c
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2021-09-09 18:34:53 +0500

    remove

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider as ThemeProvider } from "./Context/Theme";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
