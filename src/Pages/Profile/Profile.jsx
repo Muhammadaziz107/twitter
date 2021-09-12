c1d38ec565f0b453fea0bf4f4c20951642b5d59f
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2021-09-12 11:54:58 +0500

    Add

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import Back from "../../Components/Lib/back";
 function Profile() {
-  return <div>profile</div>;
+  return (
+    <div className="profile-wrapper">
+      <div className="profile">
+        <header className="profile__header">
+          <button type="button">
+            <Back />
+          </button>
+        </header>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
