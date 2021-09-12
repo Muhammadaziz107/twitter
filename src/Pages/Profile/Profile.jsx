3c56d8d1d47c433f6e6d1ee7ee54d043f675fc5a
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2021-09-12 12:14:46 +0500

    Add

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Back from "../../Components/Lib/back";
+import "./Profile.css";
 function Profile() {
   return (
     <div className="profile-wrapper">
       <div className="profile">
         <header className="profile__header">
-          <button type="button">
+          <button className="back-btn" type="button">
             <Back />
           </button>
+
+          <span className="header__wrapper">
+            <h3 className="header__username">Bobur</h3>
+            <p className="header__password">1,070 Tweets</p>
+          </span>
         </header>
       </div>
     </div>
